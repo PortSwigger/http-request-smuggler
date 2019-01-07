244efa5b78450cf83a5d269dc8d5e6cdea96f3a6
James Kettle <albinowax@gmail.com>
2019-01-07 13:55:29 +0000

    Avoid retries of smuggling payloads

--- src/burp/SmuggleScan.java
+++ src/burp/SmuggleScan.java
@@ -16,7 +16,7 @@ class SmuggleHelper {
     SmuggleHelper(IHttpService service) {
         this.service = service;
         String url = service.getProtocol()+"://"+service.getHost()+":"+service.getPort();
-        this.engine = new ThreadedRequestEngine(url, 1, 10, 1, 10, this::callback, 10);
+        this.engine = new ThreadedRequestEngine(url, 1, 10, 1, 10, 0, this::callback, 10);
     }
 
     void queue(String req) {
