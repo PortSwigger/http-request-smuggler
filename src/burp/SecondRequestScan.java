31e78284c5a5dcba060fd7be4b520ba15d51a7bb
James Kettle <albinowax@gmail.com>
2021-11-15 16:04:54 +0000

    Tweak secondReq - use host-header reflection

--- src/burp/SecondRequestScan.java
+++ src/burp/SecondRequestScan.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 
 import static burp.Utilities.getPathFromRequest;
+import static burp.Utilities.helpers;
 
 public class SecondRequestScan extends SmuggleScanBox implements IScannerCheck {
 
@@ -26,14 +27,17 @@ public class SecondRequestScan extends SmuggleScanBox implements IScannerCheck {
         original = Utilities.addOrReplaceHeader(original, "User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36");
         original = Utilities.addOrReplaceHeader(original, "Connection", "keep-alive");
 
-        final String TRIGGER = "FOO BAR AAH\r\n\r\n";
+        final String TRIGGER = helpers.bytesToString(Utilities.addOrReplaceHeader(original, "Host", "uxvmn4wyh3qhearug6zdhs2u8lee23.psres.net"));
 
         Resp bad = request(service, TRIGGER.getBytes(), 0, true);
+        if (Utilities.contains(bad, "Incapsula incident ID")) {
+            return false;
+        }
         String nonNestedCode = getPathFromRequest(bad.getReq().getResponse());
 
         TurboHelper helper = new TurboHelper(service, true);
         helper.queue(new String(original));
-        helper.queue(TRIGGER);
+        helper.queue(TRIGGER.replace("uxvmn4wyh3qhearug6zdhs2u8lee23.psres.net", "8w50mivcghpvdoq8fkyrg6187zdt1i.psres.net"));
         List<Resp> results = helper.waitFor();
         if (results.size() < 2) {
             return false;
@@ -48,10 +52,14 @@ public class SecondRequestScan extends SmuggleScanBox implements IScannerCheck {
             return false;
         }
 
-        String naturalNested = String.valueOf(results.get(1).getStatus());
-        if (naturalNested.equals(nonNestedCode)) {
+        if (Utilities.countMatches(bad, "psres.net") == Utilities.countMatches(results.get(1), "psres.net")) {
             return false;
         }
+
+//        String naturalNested = String.valueOf(results.get(1).getStatus());
+//        if (naturalNested.equals(nonNestedCode)) {
+//            return false;
+//        }
 
         //if (Utilities.getHeader(results.get(0).getReq().getResponse()))
 
@@ -59,8 +67,8 @@ public class SecondRequestScan extends SmuggleScanBox implements IScannerCheck {
         // byte[] brokenAttackReq = Utilities.replace(attackReq, "Content-Length", "Content-Cake");
 
         // todo add repeats to prevent random-code FPs
-        String title = "Second-request diff: "+nonNestedCode+":"+naturalNested;
-        report(title, "", bad, results.get(0), results.get(1));
+        String title = "Second-request diff ref: "+nonNestedCode;//+naturalNested;
+        report(title, "", original, bad, results.get(1));
         return true;
     }
 
