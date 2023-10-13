fa9b3767a711c20e74961eaf07fa5fd20c94384a
James Kettle <james.kettle@portswigger.net>
2023-10-13 09:00:20 +0100

    Bump version to 2.15

--- src/burp/BurpExtender.java
+++ src/burp/BurpExtender.java
@@ -5,7 +5,7 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class BurpExtender implements IBurpExtender, IExtensionStateListener {
     static final String name = "HTTP Request Smuggler";
-    private static final String version = "2.14";
+    private static final String version = "2.15";
     public boolean unloaded = false;
     static ConcurrentHashMap<String, Boolean> hostsToSkip = BulkScan.hostsToSkip;
 
